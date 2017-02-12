664dbc485ef0004fa70507ddc073e56b88cdcb4f
Edwin Flores <eflores003@gmail.com>
2017-02-12 05:18:57 +0000

    Built the about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,6 +12,8 @@ class QuotesController < ApplicationController
       flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
     end
     redirect_to root_path
+  end
+  def about
   end  
 
   private
